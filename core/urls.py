5415383401b0737c3924354f47450ca8646d58e2
rahat213739 <108364543+RahatBinOamr@users.noreply.github.com>
2025-02-03 23:08:23 +0600

    working on about templates and fixed on active nav-item

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import homePage
+from .views import homePage,aboutPage
 
 urlpatterns = [
     path('', homePage, name='home'),
+    path('about/',aboutPage, name='about'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def homePage(request):
-  
   return render(request, 'home.html')
+
+def aboutPage(request):
+  return render(request, 'about.html')
